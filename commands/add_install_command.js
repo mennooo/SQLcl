9fe09b502acf57f4fa3fbbef6bb2db52192f8e0c
menno <menno.hoogendijk@gmail.com>
2017-04-05 22:28:19 +0200

    no message

--- commands/add_install_command.js
+++ commands/add_install_command.js
@@ -8,7 +8,7 @@ var gCommand = "install";
 var command = loadWithNewGlobal("C:/Users/mhoogendijk/Documents/sqlcl/lib/command.js");
 
 // Library to install releases
-config = loadWithNewGlobal("https://raw.githubusercontent.com/mennooo/sqlcl/master/conf/config.js?v=7");
+var config = loadWithNewGlobal("https://raw.githubusercontent.com/mennooo/sqlcl/master/conf/config.js?v=7");
 var release = loadWithNewGlobal("C:\\Users\\mhoogendijk\\Documents\\sqlcl\\lib\\release.js",config);
 
 // Set the baseDir
