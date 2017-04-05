571ed224029686e60510a0960c3bfd5d0c18229e
menno <menno.hoogendijk@gmail.com>
2017-04-05 22:42:02 +0200

    no message

--- conf/config.js
+++ conf/config.js
@@ -1,50 +1,50 @@
-var objectTypes = {
-    script: {
-        extension: "sql",
-        versioning: false,
-        dbType: null
-    },
-    view: {
-        extension: "vw",
-        versioning: false,
-        dbType: "view"
-    },
-    packageSpec: {
-        extension: "pks",
-        versioning: true,
-        dbType: "package"
-    },
-    packageBody: {
-        extension: "pkb",
-        versioning: true,
-        dbType: "package"
-    },
-    typeSpec: {
-        extension: "typ",
-        versioning: false,
-        dbType: "type"
-    },
-    typeBody: {
-        extension: "tyb",
-        versioning: false,
-        dbType: "type"
-    },
-    trigger: {
-        extension: "trg",
-        versioning: false,
-        dbType: "trigger"
-    },
-    procedure: {
-        extension: "prc",
-        versioning: false,
-        dbType: "procedure"
-    }
-}
-
 (function() {
 
+    var objectTypes = {
+        script: {
+            extension: "sql",
+            versioning: false,
+            dbType: null
+        },
+        view: {
+            extension: "vw",
+            versioning: false,
+            dbType: "view"
+        },
+        packageSpec: {
+            extension: "pks",
+            versioning: true,
+            dbType: "package"
+        },
+        packageBody: {
+            extension: "pkb",
+            versioning: true,
+            dbType: "package"
+        },
+        typeSpec: {
+            extension: "typ",
+            versioning: false,
+            dbType: "type"
+        },
+        typeBody: {
+            extension: "tyb",
+            versioning: false,
+            dbType: "type"
+        },
+        trigger: {
+            extension: "trg",
+            versioning: false,
+            dbType: "trigger"
+        },
+        procedure: {
+            extension: "prc",
+            versioning: false,
+            dbType: "procedure"
+        }
+    }
+
     return {
-      objectTypes: objectTypes
+        objectTypes: objectTypes
     };
 
 })();
