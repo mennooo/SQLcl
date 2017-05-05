8064c19d5a32320486fb9186b1bba6ad746395d8
menno <menno.hoogendijk@gmail.com>
2017-05-05 02:35:08 +0200

    start of addobject

---
+++ demos/2_custom_commands/addobject/add_addobject_command.js
@@ -0,0 +1,44 @@
+// file: add_install_command.js
+
+// Runs on startup
+var gCommand = "addobject";
+
+// library to add a custom command
+var command = load(config.baseDir + "lib/command.js");
+
+// Library to install releases
+var release = load(config.baseDir + "lib/addobject.js");
+
+
+// Possible commands
+// install list: list the possible releases
+// install check <release>: check if the release complies
+// install release <release> start the installation
+
+command.add({
+    command: gCommand,
+    info: '\nInstall new releases. The following commands are available:\nTo set the base directory: install dir <base_dir>\nTo list all releases: install list\nTo install a release: install release <release>\nTo validate an install: install check <release>\nTo change the output: install output default|file|string|web\n\n',
+    actions: [{
+            name: "list",
+            action: release.listAll
+        },
+        {
+            name: "check",
+            action: release.check
+        },
+        {
+            name: "release",
+            action: release.install
+        },
+        {
+          name: "dir",
+          action: release.setBaseDir
+        },
+        {
+          name: "output",
+          action: release.setOutputMethod
+        }
+    ],
+    begin: function() {},
+    end: function() {}
+});
